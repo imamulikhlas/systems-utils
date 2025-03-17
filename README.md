1cacad3d64e6fc052984e04f0508025960f61bb8
imam <imamulikhlas@gmail.com>
2025-03-18 02:44:01 +0700

    IDK

---
+++ README.md
@@ -0,0 +1,8 @@
+# System Utils for Laravel
+
+A system utility package for Laravel applications.
+
+## Installation
+
+```bash
+composer require alexafers/system-utils
---
+++ src/Middleware/PreventDebugMode.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vendor\SystemUtils\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class PreventDebugMode
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Check if system is valid and in production
+        if (!system_utils_is_valid() && !App::hasDebugModeEnabled()) {
+            // Prevent debug mode from being enabled
+            config(['app.debug' => false]);
+        }
+        
+        return $next($request);
+    }
+}
